21cfeb740297cc6946fb4e21f3775f20b5b1795a
Sandra <listerbernice@gmail.com>
2023-11-26 18:15:56 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+# Inside sequences.py in the lib folder
+
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    print(fibonacci_sequence)
+
+# Test your function with print_fibonacci(9)
+print_fibonacci(9)
